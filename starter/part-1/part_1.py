8ed246b54f63677d53f9fbe255cae847d6901fe8
Brady <kite.alva@gmail.com>
2022-10-10 15:52:04 -0600

    part one done

---
+++ starter/part-1/part_1.py
@@ -0,0 +1,59 @@
+# Let's assign variables to represent a book you enjoy.
+
+# Step 2 - Strings
+# Let's start with Python strings. Replace the "None" with the name of a book, and below the name of the author of your book.
+
+book_name = None
+author = None
+# book_name = "Dune"
+# author = "Frank Herbert"
+book_name = "bob"
+author = "your_mum"
+
+
+# Step 3 - Concatenation and f-strings
+# Use concatenation and f-strings to make a sentence about your author and book name.
+
+sentence1 = None
+# sentence1 = book_name + " is written by " + author + "."
+sentence2 = None
+# sentence2 = f"Author, {author}, wrote the book {book_name}."
+sentence1 = book_name + " is written by " + author + "."
+
+sentence2 = f"Author, {author}, wrote the book {book_name}."
+
+
+# Step 4 - Integers
+# Now let's practice Integers. Replace the "None" below with the publication date of your book.
+publication_year = None
+# publication_year = 1965
+
+publication_year = 1980
+
+
+# Step 5 - Floats
+# Now estimate what the retail value of your book would be at a store and replace the "None" below with a float value of the price. (Doesn't have to be accurate.)
+book_price = None
+# book_price = 17.99
+book_price = 18.90
+
+# Step 6 - Booleans
+# Now we will practice with booleans. Replace the "is_awesome" variable with a boolean. True if your book is awesome, False if your book is not awesome.
+is_awesome = None
+# is_awesome = True
+
+is_awesome = False
+
+
+# Step 7 - print and type functions
+# Follow the instructions, and below this line, code all of the suggested print statements and the type statements.
+# Code below
+print(book_name)
+print(author)
+print(sentence1)
+print(sentence2)
+print(publication_year)
+print(book_price)
+print(is_awesome)
+
+print(f'{author} made a book called {book_name} that was made in {publication_year} and it costs {book_price}')
